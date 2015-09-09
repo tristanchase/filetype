bcc81ae2d2731fd3445d132826cd0ecdb23dc971
Tristan Chase <tristan.m.chase@gmail.com>
2015-09-09 02:36:36 -0400

    Clean up file with line spacing and comments.

--- filetype.sh
+++ filetype.sh
@@ -5,20 +5,28 @@
 # Returns single-byte indicator for file type (like ls -F).
 
 for filename in "$@"; do
-   if [[ -d ${filename} ]]; then
+
+   if [[ -d ${filename} ]]; then	#directory
      echo -e "${filename} [/]"
-   elif [[ -x ${filename} ]]; then
+
+   elif [[ -x ${filename} ]]; then	#executable
      echo -e "${filename} [*]"
-   elif [[ -p ${filename} ]]; then
+
+   elif [[ -p ${filename} ]]; then	#pipe
      echo -e "${filename} [|]"
-   elif [[ -L ${filename} ]]; then
+
+   elif [[ -L ${filename} ]]; then	#link
      echo -e "${filename} [@]"
-   elif [[ -S ${filename} ]]; then
+
+   elif [[ -S ${filename} ]]; then	#socket
      echo -e "${filename} [=]"
-   elif [[ -f ${filename} ]]; then
+
+   elif [[ -f ${filename} ]]; then	#file
      echo -e "${filename}"
+
    else
-     echo "${filename} [?]"
+     echo -e "${filename} [?]"		#unknown
+
    fi
 done
 
