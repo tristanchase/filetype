918a932b4f9b681816eb06cf6ce9e545eb41dcd0
Tristan Chase <tristan.m.chase@gmail.com>
2019-12-13 21:57:47 -0500

    Add backslash escapes for filenames with spaces

--- filetype.sh
+++ filetype.sh
@@ -2,32 +2,35 @@
 
 # filetype
 
-# Returns single-byte indicator for file type (like ls -F).
+# Returns single-byte indicator (tab-separated and bracketed) for file type (like ls -F).  Also adds backslash escapes to filenames containing spaces.
+
+set -eu
+IFS="$(printf '\n\t')"
 
 for filename in "$@"; do
 
-   if [[ -L ${filename} ]]; then	#link
-     echo -e "${filename}\t[@] -> `readlink ${filename}`"
+	if [[ -L $filename ]]; then					#link
+		echo -e "$(echo $filename | sed 's/ /\\ /g')\t[@] -> $(readlink $filename | sed 's/ /\\ /g')"
 
-   elif [[ -d ${filename} ]]; then	#directory
-     echo -e "${filename}\t[/]"
+	elif [[ -d $filename ]]; then					#directory
+		echo -e "$(echo $filename | sed 's/ /\\ /g')\t[/]"
 
-   elif [[ -x ${filename} ]]; then	#executable
-     echo -e "${filename}\t[*]"
+	elif [[ -x $filename ]]; then					#executable
+		echo -e "$(echo $filename | sed 's/ /\\ /g')\t[*]"
 
-   elif [[ -p ${filename} ]]; then	#pipe
-     echo -e "${filename}\t[|]"
+	elif [[ -p $filename ]]; then					#pipe
+		echo -e "$(echo $filename | sed 's/ /\\ /g')\t[|]"
 
-   elif [[ -S ${filename} ]]; then	#socket
-     echo -e "${filename}\t[=]"
+	elif [[ -S $filename ]]; then					#socket
+		echo -e "$(echo $filename | sed 's/ /\\ /g')\t[=]"
 
-   elif [[ -f ${filename} ]]; then	#file
-     echo -e "${filename}"
+	elif [[ -f $filename ]]; then					#regular file
+		echo -e "$(echo $filename | sed 's/ /\\ /g')"
 
-   else
-     echo -e "${filename}\t[?]"		#unknown
+	else
+		echo -e "$(echo $filename | sed 's/ /\\ /g')\t[?]"	#unknown
 
-   fi
+	fi
 done
 
 exit 0
